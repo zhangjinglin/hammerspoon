2c9eb3692f68845ea1fde0ee8720138eb6a2b054
ZhangJinglin <zhangjinglin@gmail.com>
2026-08-19 14:00:13 +0800

    quiet audio in noon

--- modules/announcer.lua
+++ modules/announcer.lua
@@ -289,26 +289,24 @@ function announcer.init()
 
 
                     -- 午休时间完全跳过
-                    -- if not isQuietTime(now.hour, now.min) then
-                    local text = format_cn_time(
-                        now.hour,
-                        now.min
-                    )
-
-
-                    hs.execute(
-                        string.format(
-                            "say -v Binbin '%s'",
-                            text
+                    if not isQuietTime(now.hour, now.min) then
+                        local text = format_cn_time(
+                            now.hour,
+                            now.min
                         )
-                    )
 
+                        hs.execute(
+                            string.format(
+                                "say -v Binbin '%s'",
+                                text
+                            )
+                        )
+                    end
 
                     show_overlay(
                         now.hour,
                         now.min
                     )
-                    -- end
                 end
             end
         end
