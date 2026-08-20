cde04896cb328e5c7136e7cfa58580500080f560
ZhangJinglin <zhangjinglin@gmail.com>
2026-08-20 14:14:29 +0800

    fix audio switcher

--- modules/audio_switcher.lua
+++ modules/audio_switcher.lua
@@ -3,24 +3,39 @@ local MONITOR_NAME       = "27M3U"
 local PROJECTOR_NAME     = "JMGO"
 -- 定义屏幕监视器
 local lastProjectorState = nil
+local m3uDisplay         = nil
 
 local audioSwitcher      = hs.screen.watcher.new(function()
     local screens = hs.screen.allScreens()
     local isProjectorOnline = false
+    local isMonitorOnline = false
 
     -- 检查 JMGO 是否在线
     for _, screen in ipairs(screens) do
         if screen:name() == PROJECTOR_NAME then
             isProjectorOnline = true
-            break
+            hs.print("jmgo on")
+        end
+        if screen:name() == MONITOR_NAME then
+            isMonitorOnline = true
+            hs.print("27m3u on")
         end
     end
 
-    -- 状态未发生变化时直接返回，避免屏幕刷新时反复执行声音切换
-    if isProjectorOnline == lastProjectorState then
-        return
+    if isMonitorOnline then
+        local monitorAudio = hs.audiodevice.findOutputByName(MONITOR_NAME)
+        if monitorAudio then
+            monitorAudio:setDefaultOutputDevice()
+            hs.printf("音频已切换回: " .. MONITOR_NAME)
+            monitorAudio:setVolume(100)
+        end
     end
-    lastProjectorState = isProjectorOnline
+
+    -- -- 状态未发生变化时直接返回，避免屏幕刷新时反复执行声音切换
+    -- if isProjectorOnline == lastProjectorState then
+    --     return
+    -- end
+    -- lastProjectorState = isProjectorOnline
 
     -- 执行状态切换逻辑
     if isProjectorOnline then
